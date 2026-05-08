15cb6ae30a4b698f65194b5e0a9737b357ffa162
matatabi <dosken234@gmail.com>
2026-05-08 20:37:27 +0900

    first commit

---
+++ src/main/java/me/matatabi/easyBlockDisplay/EasyBlockDisplay.java
@@ -0,0 +1,17 @@
+package me.matatabi.easyBlockDisplay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyBlockDisplay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
